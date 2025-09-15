11e08c4c5b6dd3c73ddc008902807d2d70c79a2b
Matheus359660 <goncalves.santos.matheus1601@escola.pr.gov.br>
2025-09-15 14:13:21 +0000

    em andamento os códigos

--- jogoAdivinha.c
+++ jogoAdivinha.c
@@ -1,4 +1,17 @@
 # include <stdio.h>
 int main(){
-    printf("Olá mundo!");
+
+    //imprime o cabeçalho do jogo.
+    printf("****************************************\n");
+    printf("* Bem Vindo au nosso jogo de adivinhação\n");
+    printf("****************************************\n");
+
+    int numeroSecreto = 42;
+
+   //printf(" O número %d é o secreto. Não conta pra ninguém!");
+
+  int chute;
+  printf("Qual é o seu chute? ");
+  scanf("%d", &chute);
+  printf("Seu chute foi %d", chute);
 }
